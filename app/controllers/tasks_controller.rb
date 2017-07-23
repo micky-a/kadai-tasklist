b19cc3ced38c2b7469ae07c6c2404b3bbce77fe0
micky.a <mickyaokiprgrm@gmail.com>
2017-07-23 14:20:48 +0000

    saveall

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,60 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    
+  end
+  
+  def new
+    @task = Task.new
+    
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = '正常に登録されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = '登録されませんでした'
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @task.update(task_params)
+      flash[:success] = '正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = '更新されませんでした'
+      render :edit
+    end
+  end
+  
+  
+  def destroy
+    @task.destroy
+
+    flash[:success] = '正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  #共通部
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
+end
